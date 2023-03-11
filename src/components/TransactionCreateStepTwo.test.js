deef7577f80c4f98c1c7fcb96bece49d8be842be
Hitendra Quessou <sanjay_quessou@yahoo.co.uk>
2023-03-11 22:46:01 +0000

    added first unit test.

---
+++ src/components/TransactionCreateStepTwo.test.js
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
+
+test("on initial render, the pay button is disabled", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
+});
